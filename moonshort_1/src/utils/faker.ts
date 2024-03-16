6142409449a39925d51d84be6df6fc8f3c038fca
Pratham Das <prathamdas003@gmail.com>
2024-03-16 21:42:14 +0530

    (chore): Changed the Data

--- moonshort_1/src/utils/faker.ts
+++ moonshort_1/src/utils/faker.ts
@@ -7,7 +7,7 @@ type DataType = {
 export const data = (() => {
   const data: DataType[] = [];
 
-  for (let i = 0; i <= 100; i++) {
+  for (let i = 0; i < 100; i++) {
     const name = faker.commerce.product();
     const check = faker.datatype.boolean();
     data.push({ name, check });
