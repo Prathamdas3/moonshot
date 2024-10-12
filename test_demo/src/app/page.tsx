49ac578b1d46870fc367e4b5d9af77a7f55d1e1a
prathamdas <prathamdas003@gmail.com>
2024-10-12 10:31:34 +0530

    chore(app): updated the repo with my recent project

---
+++ test_demo/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from '@/components/Navbar'
+import { Footer } from '@/components/Footer'
+import { Hero } from '@/components/Hero'
+
+export default async function Page() {
+  return (
+    <main>
+      {/* <Navbar /> */}
+      <Hero />
+      {/* <Footer /> */}
+    </main>
+  )
+}
